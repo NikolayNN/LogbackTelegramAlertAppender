71e0e2c756ced1ec108dcdb4e3d98681d29b8615
NikolayNN <nikolay.horushko@gmail.com>
2022-05-24 10:27:26 +0300

    set message level err

--- src/main/java/by/nhorushko/logback/appender/ConsoleAlert.java
+++ src/main/java/by/nhorushko/logback/appender/ConsoleAlert.java
@@ -4,6 +4,6 @@ public class ConsoleAlert implements AlertMessageSendable {
 
     @Override
     public void sendMessage(String type, String message) {
-        System.out.printf("ALERT: %s : %s%n", type, message);
+        System.err.printf("ALERT: %s : %s%n", type, message);
     }
 }
